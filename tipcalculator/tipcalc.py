2df9a52c8e9e09e7243c48f9005211837066312c
kashifrahuman <288284119+kashifrauhman@users.noreply.github.com>
2026-07-22 21:47:09 +0530

    Finish Project: Tip Calculator

---
+++ tipcalculator/tipcalc.py
@@ -0,0 +1,7 @@
+print("Welcome to the tip calculator!")
+tip_total = input("What was the total bill paid?")
+tip_percinput =("How much tip would you like to give? 10, 12 or 15?")
+tip_people = ("How many people to split the bill?")
+
+tip_perc = (tip_total/tip_percinput/100)
+print(tip_perc)
