a8ef6c7c8058532cd2e5b39579c04dde5e8228e3
kashifrahuman <288284119+kashifrauhman@users.noreply.github.com>
2026-07-26 23:12:17 +0530

    Display error message upon invalid input

--- rockpaperscissors/rockpaperscissors.py
+++ rockpaperscissors/rockpaperscissors.py
@@ -26,31 +26,31 @@ scissors = '''
 userinput = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
 computer = random.randint(0,2)
 
-if userinput == 0:
-    print(rock)
-elif userinput == 1:
-    print(paper)
-elif userinput == 2:
-    print(scissors)
+if userinput >= 3:
+    print("The inputted value is invalid")
 else:
-    print("Undefined")
-print("Computer chose")
+    if userinput == 0:
+        print(rock)
+    elif userinput == 1:
+        print(paper)
+    elif userinput == 2:
+        print(scissors)
 
-if computer == 0:
-    print(rock)
-elif computer == 1:
-    print(paper)
-else:
-    print(scissors)
-if computer == 1 and userinput ==0 :
-    print("The computer won")
-elif computer == 2  and userinput == 1:
-    print("The computer won")
-elif computer == 0 and userinput == 2:
-    print("The computer won")
-elif computer == userinput:
-    print("Its a draw")
-else:
-    print("The user won")
+    print("Computer chose")
 
-
+    if computer == 0:
+        print(rock)
+    elif computer == 1:
+        print(paper)
+    else:
+        print(scissors)
+    if computer == 1 and userinput ==0 :
+        print("The computer won")
+    elif computer == 2  and userinput == 1:
+        print("The computer won")
+    elif computer == 0 and userinput == 2:
+        print("The computer won")
+    elif computer == userinput:
+        print("Its a draw")
+    else:
+        print("The user won")
